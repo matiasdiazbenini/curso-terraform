04d3ba08b4541d89fc776c80a95c940b6ae2ac2e
matiasdiazbenini <matiasdiazbenini@gmail.com>
2026-08-04 23:40:54 -0300

    last

--- cicd/main.tf
+++ cicd/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "continousdeployment"
+  name                     = "cicd-mati"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
