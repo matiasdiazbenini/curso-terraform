e97d5396c17e607862bf6d450824ba9a806e4ec9
Matias Agustin Diaz Benini <96202612+matiasdiazbenini@users.noreply.github.com>
2026-08-04 23:26:13 -0300

    Refactor Azure storage containers in main.tf
    Removed 'container_mati' and 'container_oscar' resources, added 'container_cordoba' resource.

--- cicd/main.tf
+++ cicd/main.tf
@@ -16,14 +16,8 @@ resource "azurerm_storage_account" "storage" {
   account_replication_type = "LRS"
 }
 
-resource "azurerm_storage_container" "container_mati" {
-  name                  = "platzicba-mati"
-  storage_account_id    = azurerm_storage_account.storage.id
-  container_access_type = "container"
-}
-
-resource "azurerm_storage_container" "container_oscar" {
-  name                  = "oscarbarajas"
+resource "azurerm_storage_container" "container_cordoba" {
+  name                  = "platzicordoba"
   storage_account_id    = azurerm_storage_account.storage.id
   container_access_type = "container"
 }
