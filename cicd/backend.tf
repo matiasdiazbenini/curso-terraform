9c37deffc1f21f965ca960284e6a0b2ddcda7af3
matiasdiazbenini <matiasdiazbenini@gmail.com>
2026-08-04 22:49:58 -0300

    fixed cicd

--- cicd/backend.tf
+++ cicd/backend.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "azurerm" {
-    storage_account_name = "estadosterraformamin"
-    container_name       = "githubactionstate"
+    storage_account_name = "estadosterraformmatidb"
+    container_name       = "states"
     key                  = "estados.tfstate"
+
+    use_azuread_auth = true
   }
 }
